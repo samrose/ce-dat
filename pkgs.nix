760a575021f71bc71d4d59d19a5e33c8d7a93703
samrose <samuel.rose@gmail.com>
2019-11-21 09:22:01 -0500

    pkgs.nix source update

--- pkgs.nix
+++ pkgs.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://gitlab.com/samueltrose/ce-nixpkgs/-/archive/172459c2425c0f855c2ba12e6d3f645110aaa9bf/ce-nixpkgs-master.tar.gz";
+  url = "https://github.com/samrose/ce-nixpkgs/archive/172459c2425c0f855c2ba12e6d3f645110aaa9bf.tar.gz";
   sha256 = "1fm9qk0pnqv6bakn3mslbb4x3gmz62g237viw7sn2kayipd649k1";
 })
